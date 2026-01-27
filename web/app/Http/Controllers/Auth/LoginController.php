b614b56ce487d631a79e299f3e1c843c33f633c5
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-27 15:21:31 +0100

    feat: add LoginController for user authentication

---
+++ web/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function Login($request)
+    {
+     
+    }
+}
