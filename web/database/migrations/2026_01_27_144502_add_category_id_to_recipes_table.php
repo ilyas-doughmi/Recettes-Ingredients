2bdfcc2b76e8f4af90f1da729c2a775a3f0aadf9
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-27 16:13:45 +0100

    feat: add category_id foreign key to recipes table

---
+++ web/database/migrations/2026_01_27_144502_add_category_id_to_recipes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('recipes', function (Blueprint $table) {
+            $table->foreignId('categorie_id')
+                    ->after('id')
+                    ->constrained('categories')
+                    ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('recipes', function (Blueprint $table) {
+            $table->dropForeign(['categorie_id']);
+            $table->dropColumn(['categorie_id']);
+        });
+    }
+};
