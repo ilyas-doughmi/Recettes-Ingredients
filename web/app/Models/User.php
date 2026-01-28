21bfd3ef70f028949b9e558bc28e53e1212a5834
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-28 16:58:48 +0100

    feat: add relationship method for user to recipes

--- web/app/Models/User.php
+++ web/app/Models/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function recipes()
+    {
+        return $this->hasMany(Recipe::class, 'user_id');
+    }
 }
