de683bf1ab8ee897946c72e3a82def1239c05094
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-28 17:01:22 +0100

    feat: create CategorieController with index method for category management

---
+++ web/app/Http/Controllers/CategorieController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Container\Attributes\Auth;
+
+class CategorieController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
